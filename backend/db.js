434dcbfb744db6ebfe5eecc155e8b36988f3be99
Vivek Patel <vp133202@gmail.com>
2023-03-27 16:59:24 +0530

    change

--- backend/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
-
-const connection = mongoose.connect("mongodb+srv://svivek:sita@cluster1.is5oxku.mongodb.net/linkedin?retryWrites=true&w=majority")
-
-module.exports = {
-    connection
-}
